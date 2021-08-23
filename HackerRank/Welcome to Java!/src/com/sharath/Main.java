8cfc5308c3640f47654c312a733807e18313ab39
sharathnair007 <sharathnair01.ss@gmail.com>
2021-08-23 16:33:31 +0530

    my first java program

---
+++ HackerRank/Welcome to Java!/src/com/sharath/Main.java
@@ -0,0 +1,9 @@
+package com.sharath;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
