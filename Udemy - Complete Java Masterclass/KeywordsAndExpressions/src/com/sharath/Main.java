582d05f2d9c53b2d584e9315d928a2641976506e
sharathnair007 <sharathnair01.ss@gmail.com>
2021-08-23 16:39:10 +0530

    keywords and expressisons

---
+++ Udemy - Complete Java Masterclass/KeywordsAndExpressions/src/com/sharath/Main.java
@@ -0,0 +1,22 @@
+package com.sharath;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Java has 50 reserved built-in keywords.
+        // Challenge
+        // In the following code below, write down the parts of the code that are expressions.
+        int score = 100;
+        if (score > 99){
+            System.out.println("You got the high score!");
+            score = 0;
+
+        // so here the following are the expressions from the above code.
+        // score = 100
+        // score > 99
+        // score = 0
+        }
+
+    }
+}
