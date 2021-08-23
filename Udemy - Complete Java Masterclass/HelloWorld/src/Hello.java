bdcd1856186982a69146793bf7dc46a7505d00b2
sharathnair007 <sharathnair01.ss@gmail.com>
2021-08-23 16:38:28 +0530

    first java program

---
+++ Udemy - Complete Java Masterclass/HelloWorld/src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello, Sharath!");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
+    }
+}
